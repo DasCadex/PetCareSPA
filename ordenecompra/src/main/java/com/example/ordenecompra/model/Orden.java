5505b58c51d6e592a655ace85fc48b3695871637
Fernando gabriel esteban <fe.villalobosj@duocuc.cl>
2025-05-22 00:47:23 -0400

    actualizaciones de modulo y nuevo modulo
    ajuste de mosulos deusuario y oeden de compra eh implementacion de productos

---
+++ ordenecompra/src/main/java/com/example/ordenecompra/model/Orden.java
@@ -0,0 +1,53 @@
+package com.example.ordenecompra.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name= "ordenes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class Orden {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_orden;
+
+
+    @Column(nullable = false )
+    private Long usuarioId;
+
+ 
+
+    @Column(nullable = false )
+    private Long productoId;
+
+    @Column(nullable = false)
+    private String nombreProducto;
+
+    @Column(nullable = false)
+    private String nombres;
+
+    @Column(nullable= false )
+    private Double precioProducto;
+
+    @Column(nullable = false )
+    private Integer cantidad;
+
+
+
+
+
+    
+
+}
---
+++ ordenecompra/src/main/java/com/example/ordenecompra/service/OrdenService.java
@@ -0,0 +1,108 @@
+package com.example.ordenecompra.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.management.RuntimeErrorException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ordenecompra.model.Orden;
+import com.example.ordenecompra.repository.OrdenRepository;
+import com.example.ordenecompra.webclient.ProductoClient;
+import com.example.ordenecompra.webclient.UsuarioClient;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class OrdenService {
+
+    @Autowired
+    private OrdenRepository ordenRepository;
+
+    @Autowired
+    private UsuarioClient usuarioClient;
+
+    @Autowired
+    private ProductoClient productoClient;
+
+    
+    public List<Orden> buscarOrden() {
+        return ordenRepository.findAll();
+    }
+
+    public Orden guardarOrdenCompre(Orden nuevaOrden) {
+
+        Map<String, Object> usuario = usuarioClient.getUsuarioById(nuevaOrden.getUsuarioId());
+        if (usuario == null || usuario.isEmpty()) {
+            throw new RuntimeException("Cliente no encontrado, no se puede crear la orden de compra");
+        }
+
+     
+        Map<String, Object> producto = productoClient.getProductoById(nuevaOrden.getProductoId());//obtienes el id del prodcuto llamando a la clase  y lo guarda y si no existe dara el error 
+
+        if (producto == null || producto.isEmpty()) {
+            throw new RuntimeException("Producto no encontrado, no se puede crear la orden de compra");
+        }
+
+
+        String nombreProducto = (String) producto.get("nombre_producto"); //le damos la key pára saber que datos quiere sacar 
+        if (nombreProducto == null) {
+            throw new RuntimeException("El producto no contiene un campo 'nombre'");
+        }
+
+        nuevaOrden.setNombreProducto(nombreProducto);//guardamos el nombre del porducto que llamos 
+
+        Object preciObj  = producto.get("precio_producto");
+        if( preciObj==null ){//llamo a la clase que tenga el precio producto 
+            throw new RuntimeException("el prodcuto no tiene precio ");
+        }
+        
+          Double precioProducto;
+         try{
+            precioProducto= Double.valueOf(preciObj.toString());//si es sztring lo convertimos adecimal 
+            
+
+        }catch (NumberFormatException e){
+            throw new RuntimeException("Error al convertir el precio del producto a número");
+
+        }
+        nuevaOrden.setPrecioProducto(precioProducto); 
+        
+         
+
+ 
+        String nombres = (String) usuario.get("nombres");
+        if(nombres==null){
+            throw new RuntimeException("el usuario no tiene hombres ");
+        }
+        nuevaOrden.setNombres(nombres);
+
+        
+      
+        if (nuevaOrden.getNombreProducto() == null || nuevaOrden.getNombreProducto().isEmpty()) {
+            throw new RuntimeException("El nombre del producto no puede ser nulo o vacío");
+        }
+
+        
+        return ordenRepository.save(nuevaOrden);
+        
+    }
+
+    public void eliminarOrden(Long id_orden) {
+        Orden orden = ordenRepository.findById(id_orden).orElseThrow(() -> new RuntimeException("Orden no encontrada"));
+        ordenRepository.delete(orden);
+    }
+
+    public Double calcularTotalOrdenes() {
+    List<Orden> ordenes = ordenRepository.findAll();// esto nos permite calcular el valor de los productos y el precio 
+    return ordenes.stream()
+            .mapToDouble(orden -> orden.getPrecioProducto() * orden.getCantidad())
+            .sum();
+}
+
+
+
+}
