69f95f82a2f4ba68a793412cd6d40b9ba5fbf721
NikitoProRex777 <ni.sotomayor@duocuc.cl>
2025-05-23 01:36:25 -0400

    Pete el historial  :v
    Peto, estaba todo rojo xd

--- Citas/src/main/java/com/example/Citas/Repository/CitasRepository.java
+++ Citas/src/main/java/com/example/Citas/Repository/CitasRepository.java
@@ -1,5 +1,12 @@
 package com.example.Citas.Repository;
 
-public interface CitasRepository {
+import com.example.Citas.Model.Citas;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface CitasRepository extends JpaRepository<Citas, Long> {
 
 }
